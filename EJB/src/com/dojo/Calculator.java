0adda9e262bbe60708611d85206f8d5dba2c2ee7
novvvi <novehui@gmail.com>
2020-05-05 15:12:24 -0500

    EJB project

--- EJB/src/com/dojo/Calculator.java
+++ EJB/src/com/dojo/Calculator.java
@@ -1,4 +1,42 @@
 package com.dojo;
 
-public class Calculator {
+public class Calculator implements java.io.Serializable{
+    private int OperandOne;
+    private int OperandTwo;
+    private String Operation;
+
+    public Calculator() {
+
+    }
+
+    public int performOperation() {
+        if(Operation.equals("+")) {
+            return OperandOne + OperandTwo;
+        }
+        else if(Operation.equals("-")) {
+            return OperandOne - OperandTwo;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public void getResults() {
+        System.out.println(this.performOperation());
+    }
+
+    public void setOperandOne(int operandOne) {
+        OperandOne = operandOne;
+    }
+
+    public void setOperandTwo(int operandTwo) {
+        OperandTwo = operandTwo;
+    }
+
+    public void setOperation(String operation) {
+        Operation = operation;
+    }
+
+
+
 }
---
+++ EJB/src/com/dojo/Main.java
@@ -0,0 +1,8 @@
+package com.dojo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
